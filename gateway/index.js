87b561a1837d9180ef9ee66c70fb9a4623e6ddf4
EmanFun <emanuelfun.95@gmail.com>
2023-05-26 13:26:47 -0300

    added proxys

---
+++ gateway/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const {createProxyMiddleware} = require('http-proxy-middleware');
+
+const app = express();
+
+app.use('/characters', createProxyMiddleware({
+  target: "http://characters:8001",
+  changeOrigin: true,
+}))
+
+app.use('/films', createProxyMiddleware({
+  target: 'http://films:8002',
+  changeOrigin: true,
+}))
+
+app.use('/planets', createProxyMiddleware({
+  target: 'http://planets:8003',
+  changeOrigin: true,
+}))
+
+app.listen(8000, () => {
+  console.log("Server listening on port 8000");
+});
